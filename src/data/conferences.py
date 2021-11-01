c660b1ba49ea284abbebd9ca39e3b1ce0c4f14d4
ZzAZz4 <esteban.villacorta@utec.edu.pe>
2021-11-01 16:17:32 -0500

    handled case where endHour is not defined

--- src/data/conferences.py
+++ src/data/conferences.py
@@ -1,7 +1,12 @@
 from dataclasses import dataclass
-from datetime import datetime
+from datetime import datetime, timedelta
 
-
+def calculate_end_hour(date, end_hour, fallback=None):
+    if end_hour == None:
+        end_time = fallback + timedelta(hours=2)
+    else:
+        end_time = datetime.fromisoformat(f"{date} {end_hour}")
+    return end_time
 @dataclass
 class Conference:
     course: str
@@ -11,7 +16,6 @@ class Conference:
 
     @staticmethod
     def from_json(json_data):
-        
         conference_data = json_data['conference']
         url = json_data['joinUrl']
         date = json_data['startTime']
@@ -21,6 +25,8 @@ class Conference:
         end_hour = conference_data['endHour']
 
         start_time = datetime.fromisoformat(f"{date} {start_hour}")
-        end_time = datetime.fromisoformat(f"{date} {end_hour}")
+        
+        end_time = calculate_end_hour(date, end_hour, fallback=start_time)
 
         return Conference(course, start_time, end_time, url)
+
